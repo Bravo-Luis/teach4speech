3d1a5600f206ac7a0b993e4310ff4499bab65b13
Bravo-Luis <luisbravo@ucsb.edu>
2023-12-23 15:43:54 -0800

    added timer and textmui

--- frontend/src/Game.tsx
+++ frontend/src/Game.tsx
@@ -1,23 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import TextField from '@mui/material/TextField';
 function Game(){
 
-
     const [inputText, setInputText] = useState("");
+    const [timer, setTimer] = useState(0);
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            setTimer((timer)=>timer+1);
+        }, 1000);
+        return ()=>clearInterval(interval);
+    }, []);
+
+    console.log(timer)
+
+    function handleSubmit(potenialAnswer: string) { 
+        // placeholder for api call
+        return true // or false
+    }
 
     return (
         <>
-
         <h1>Game</h1>
-        
         <input type="text" placeholder="Enter your name"  onChange={(event)=>{
             setInputText(event.target.value);
         }} />
-
         <button onClick={()=>{
             console.log(inputText);
         }}> Submit </button>
-
         </>
     )
 }
