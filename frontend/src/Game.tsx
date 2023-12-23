1d6faae536cc512680176e85b661805f073e7730
Bravo-Luis <luisbravo@ucsb.edu>
2023-12-23 15:29:27 -0800

    game stuff

--- frontend/src/Game.tsx
+++ frontend/src/Game.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 function Game(){
+
+
+    const [inputText, setInputText] = useState("");
+
     return (
         <>
-        This is the content of the game
+        <h1>Game</h1>
+        <input type="text" placeholder="Enter your name"  onChange={(event)=>{
+            setInputText(event.target.value);
+        }} />
+        <button onClick={()=>{
+            console.log(inputText);
+        }}>Submit</button>
         </>
     )
 }
