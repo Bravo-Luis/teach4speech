777c0b9491ca27ece3f477c4c7470f996c3864e9
Brandon Mercado <brandonmercado103@gmail.com>
2024-01-13 01:47:34 -0800

    including react audio voice recorder

--- src/Game2.tsx
+++ src/Game2.tsx
@@ -1,20 +1,52 @@
-import './Game2.css'
+import React, { useState } from 'react';
+import './Game2.css';
+import { AudioRecorder } from 'react-audio-voice-recorder';
 
-function Game2(){
+function Game2() {
+  const [recordings, setRecordings] = useState<Blob[]>([]);
 
-    // From react side: recording the voice messages, store them locally, done button to send to back end
-    // Buttons: Record, listen to recording again, redo recording, done
+  const addAudioElement = (blob: Blob) => {
+    setRecordings((prevRecordings) => [...prevRecordings, blob]);
+    const url = URL.createObjectURL(blob);
+    const audio = document.createElement('audio');
+    audio.src = url;
+    audio.controls = true;
+    document.body.appendChild(audio);
+    console.log("Adding to array")
+  };
 
-    return (
-        <div className='game-2'>
-            <button id="Record">Record word</button>
-            <div className="buttons">
-                <button id="Listen">Listen to recording</button>
-                <button id="Redo">Redo recording</button>
-                <button id="Done">Send recording</button>
-            </div>
-        </div>
-    )
+  const saveRecordingLocally = () => {
+    // Storing the blob in the recordings array
+    if (recordings.length > 0) {
+      console.log("Newly recorded audio blob:", recordings[recordings.length - 1]);
+    }
+  };
+
+  const playRecording = () => {
+    if (recordings.length > 0) {
+      const combinedBlob = new Blob(recordings, { type: 'audio/webm' });
+      const url = URL.createObjectURL(combinedBlob);
+      const audio = new Audio(url);
+      audio.play();
+    }
+  };
+
+  return (
+    <div className="game-2">
+      <div id="recordButton">
+        <AudioRecorder onRecordingComplete={addAudioElement} showVisualizer />
+      </div>
+
+      <div>
+        {recordings.length > 0 && (
+          <div>
+            <button onClick={saveRecordingLocally}>Save Recording Locally</button>
+            <button onClick={playRecording}>Play Combined Recording</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Game2;
