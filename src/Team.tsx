3b9182d701817e821a7cba45e7893d218c341f17
Brandon Mercado <brandonmercado103@gmail.com>
2024-04-16 22:23:14 -0700

    adding names, descriptions and avatars to profile cards.
    New folder created inside of assets called Team_Headshots (sorry if that capitalization isn't good practice, you can rename it if it bothers you) containing png images of everyone's avatars.

--- src/Team.tsx
+++ src/Team.tsx
@@ -3,6 +3,15 @@ import { Typography, Container, Box, Avatar } from '@mui/material';
 import { grey } from '@mui/material/colors';
 import Tilt from 'react-parallax-tilt';
 
+import andres from './assets/Team_Headshots/andres.png';
+import anna from './assets/Team_Headshots/anna.png';
+import benicio from './assets/Team_Headshots/benicio.png';
+import brandon from './assets/Team_Headshots/brandon.png';
+import eric from './assets/Team_Headshots/eric.png';
+import jayne from './assets/Team_Headshots/jayne.png';
+import olivia from './assets/Team_Headshots/olivia.png';
+import riya from './assets/Team_Headshots/riya.png';
+
 interface ProfileCardProps {
     name: string;
     description: string;
@@ -25,10 +34,66 @@ function Team(){
             justifyContent:'center',
             gap:'2vw',
         }}>
-            <ProfileCard name="Luis" description="A passionate software engineer who developed this website!" avatarUrl="https://tr.rbxcdn.com/d5a7a6b9ac9fe4b94c6d8a82476d6a1f/420/420/Hat/Png" />
-            <ProfileCard name="Luis" description="A passionate software engineer who developed this website!" avatarUrl="https://tr.rbxcdn.com/d5a7a6b9ac9fe4b94c6d8a82476d6a1f/420/420/Hat/Png" />
-            <ProfileCard name="Luis" description="A passionate software engineer who developed this website!" avatarUrl="https://tr.rbxcdn.com/d5a7a6b9ac9fe4b94c6d8a82476d6a1f/420/420/Hat/Png" />
-            <ProfileCard name="Luis" description="A passionate software engineer who developed this website!" avatarUrl="https://tr.rbxcdn.com/d5a7a6b9ac9fe4b94c6d8a82476d6a1f/420/420/Hat/Png" />
+            <ProfileCard 
+                name="Riya Nilkant" 
+                description="Riya founded the Teach4Speech program creating a tailored curriculum for speaking and advocacy at local Santa Barbara elementary schools. She is a Biological Sciences major at UCSB passionate about medicine and life sciences research. In her free time, she enjoys exploring the outdoors and new coffee shops!" 
+                avatarUrl={riya}
+            />
+
+            <ProfileCard 
+                name="Andres Stidger" 
+                description="I am a 3rd year student at UCSB and I enjoy freediving and rock climbing in my free time. I volunteer as an in-person educator for Teach4Speech, helping children foster their communication skills." 
+                avatarUrl={andres} 
+            />
+
+            <ProfileCard 
+                name="Olivia Candelaria" 
+                description="TBD" 
+                avatarUrl={olivia} 
+            />
+
+            <ProfileCard 
+                name="Benicio Rivera" 
+                description="I’m Benicio a third year applied mathematics student and am a part of the curriculum team for Teach4Speech. I am passionate about teaching and working on automotives." 
+                avatarUrl={benicio}
+            />
+
+            <ProfileCard 
+                name="Eric Lee" 
+                description="Eric is a master’s student at Stanford studying Computer Science and helps manage development initiatives for the technology team. In his free time, he loves to cook and play violin!" 
+                avatarUrl={eric} 
+            />
+
+            <ProfileCard 
+                name="Brandon Mercado" 
+                description="Hi, I’m Brandon and I’m part of the Game Development department here at T4S! One of my favorite hobbies is going bowling because I’m pretty good at it." 
+                avatarUrl={brandon}
+            />
+
+            <ProfileCard 
+                name="Luis" 
+                description="A passionate software engineer who developed this website!" 
+                avatarUrl="https://tr.rbxcdn.com/d5a7a6b9ac9fe4b94c6d8a82476d6a1f/420/420/Hat/Png" 
+            />
+
+            <ProfileCard 
+                name="Julianna" 
+                description="TBD" 
+                avatarUrl="https://tr.rbxcdn.com/d5a7a6b9ac9fe4b94c6d8a82476d6a1f/420/420/Hat/Png" 
+            />
+
+            <ProfileCard 
+                name="Anna Leith" 
+                description="I’m anna, I’m a 3rd year psych and brain science major at ucsb and I am part of the t4s outreach and administration teams. In my free time I enjoy hiking, slack lining, and working with neurodiverse populations." 
+                avatarUrl={anna}
+            />
+
+            <ProfileCard 
+                name="Jayne Romero" 
+                description="TBD" 
+                avatarUrl={jayne}
+            />
+
 
         </Container>
         </>
