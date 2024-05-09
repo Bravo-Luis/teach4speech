06201df514ab2e61f7077101a6155da4c60e7f0e
Bravo-Luis <luisbravo@ucsb.edu>
2024-05-08 20:20:31 -0700

    fixing impact text

--- src/components/landing/About/About.tsx
+++ src/components/landing/About/About.tsx
@@ -24,13 +24,13 @@ function About() {
     {
       id : "Goal",
       header: "Our Mission",
-      text: "{Teach4Speech} provides a program geared at providing elementary and middle school students from low-income and underrepresented backgrounds with speech and communication skills. In equipping students with the tools to find their {voice}, build {confidence}, and advocate for themselves, we hope to {inspire} the next generation of {leaders}, and open opportunities in a community that does not have access to this.",
+      text: "Teach4Speech provides a program geared at providing elementary and middle school students from low-income and underrepresented backgrounds with speech and communication skills. In equipping students with the tools to find their voice, build confidence, and advocate for themselves, we hope to inspire the next generation of leaders, and open opportunities in a community that does not have access to this.",
       link: "/ourmission",
     },
     {
       id: "Founder",
       header: "What is Teach4Speech? - A Message From Our Founder ",
-      text: "Teach4Speech is an organization that aims to bolster {speech} and {communication} skills in underrepresented elementary school students. While tutoring at local schools in Goleta, I saw firsthand how few of the students envisioned themselves in {higher} {education}, and how even fewer possessed the ability to {advocate} for themselves in the current education system. Many of the students were failing middle school and unable to advance to high school because they could not self-advocate to their teachers to meet graduation requirements. Much of the richness in my education came from the skillsets I acquired through speech and {debate}. With this in mind, I developed a curriculum that would build confidence in and {inspire} the next generation of {leaders}.",
+      text: "Teach4Speech is an organization that aims to bolster speech and communication skills in underrepresented elementary school students. While tutoring at local schools in Goleta, I saw firsthand how few of the students envisioned themselves in higher education, and how even fewer possessed the ability to advocate for themselves in the current education system. Many of the students were failing middle school and unable to advance to high school because they could not self-advocate to their teachers to meet graduation requirements. Much of the richness in my education came from the skillsets I acquired through speech and debate. With this in mind, I developed a curriculum that would build confidence in and inspire the next generation of leaders.",
       link: "/message",
       quoted: "- Riya Nilkant, Founder and Executive Director"
     }
@@ -97,7 +97,7 @@ function About() {
        
       </Box>
       <Box sx={{ display: "flex", flexDirection: "row", flexWrap: "wrap-reverse", alignItems:"center", justifyContent:"center", gap:"2.5vw", padding:"2.5vw" }}>
-        <ImpactText text=" After just one week of the program, students of {Hispanic} backgrounds reported a {16%} {increase} in confidence for speaking in small groups. In public speaking, confidence increased by {8%} and speaking to adults increased by {7%}."/>
+        <ImpactText text=" After just one week of the program, students of Hispanic backgrounds reported a 16% increase in confidence for speaking in small groups. In public speaking, confidence increased by 8% and speaking to adults increased by 7%."/>
         <img
           src={ConfHispanic}
           alt="Confidence Levels of Hispanic Students"
