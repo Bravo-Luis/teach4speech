803a2eaa6f18e68780dd363b5b84fed688e5c64d
Brandon Mercado <brandonmercado103@gmail.com>
2024-05-22 01:54:13 -0700

    Adding write-ups/blurbs sections

---
+++ src/components/landing/Games/GamesStyles.ts
@@ -0,0 +1,22 @@
+import { makeStyles } from "@mui/styles";
+import theme from "../../../styles/Theme";
+
+const useGamesStyles = makeStyles(() => ({
+  ImpactBubbleStyling: {
+    border: "1px solid black",
+    borderRadius: "1vw",
+    padding: "1vw",
+    backgroundColor: theme.palette.primary.light,
+  },
+  ImpactBubbleCollection: {
+    marginTop: "5vh",
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap: "2vw",
+    maxWidth: "clamp(300px, 80%, 1200px)",
+  }
+}));
+
+export default useGamesStyles;
