85d31ec2d7bbacb6863408b77c50b552565ded6c
Brandon Mercado <brandonmercado103@gmail.com>
2024-06-19 02:04:48 -0700

    creating new Why Us section

--- src/components/HeaderAndText.tsx
+++ src/components/HeaderAndText.tsx
@@ -1,7 +1,11 @@
 import { Box, Button, Typography } from "@mui/material";
+import { useState } from "react";
 import ImpactText from "./ImpactText";
+import OurGoalPie from "../assets/charts/OurGoalPie.png";
+import useCommonStyles from "../styles/CommonStyles";
 
-function HeaderAndText({
+function HeaderAndText(
+  {
   header,
   text,
   link,
@@ -9,6 +13,7 @@ function HeaderAndText({
   impact = false,
   game,
   image,
+  chart = false,
 }: {
   header: string;
   text: string;
@@ -17,7 +22,18 @@ function HeaderAndText({
   impact?: boolean;
   game?: string;
   image?: any;
+  chart?: boolean;
 }) {
+
+  const commonStyles = useCommonStyles();
+  const [selectedChart] = useState(0);
+  const charts = [
+    {
+      src: OurGoalPie, 
+      alt: 'Pie chart displaying student career goals and educational pathways.', 
+      description: "From a sample of 156 students in Santa Barbara,  over 70% of students envisioned themselves in careers that either required no education or just a high school diploma.  Fewer than 25% aimed to pursue college or higher education. Georgetown University recently released a report that by 2031, 72% of jobs will require more than a high school diploma.  At Teach4Speech, we aim to bridge these gaps by fostering a love for learning early in students' educational journeys, and inspiring them to pursue high aspirations."
+    },
+  ] 
   return (
     <Box
       sx={{
@@ -47,36 +63,6 @@ function HeaderAndText({
       {(text && impact) && <ImpactText text={text} />}
       <br />
       <br />
-    
-      {link ? (
-        <Box sx={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap-reverse",
-          width: "100%",
-          justifyContent:"space-between",
-          rowGap: "2.5vh",
-        }}>
-          <Button href={link} variant="outlined"
-          sx={{
-            padding: "10px 20px",
-            fontSize: "1.2rem",
-            fontWeight: "bold",
-          }}>
-            <Typography variant="caption" color={"primary"}
-            sx={{fontSize: "1.2rem", fontWeight: "bold"}}>
-              {header === 'The Impact' ? 'Learn More About The Problem' : 'Learn More'}
-            </Typography>
-          </Button>
-          {quoted && 
-       <Typography textAlign={"right"} paddingRight={"2.5vw"}>
-        <i>{quoted}</i>
-       </Typography>
-    
-        }
-
-        </Box>
-      ) : null}
 
         {game ? (
         <Box sx={{
@@ -121,6 +107,51 @@ function HeaderAndText({
           />
         </Box>
       )}
+
+      {chart? (
+        <Box className={commonStyles.ColumnCenteredContainer} sx={{ paddingBottom: "0vh", marginLeft: "-39.5vh", marginBotton: "", /* background: gradient, */}}>
+          <Box sx={{ display: "flex", flexDirection: "row", flexWrap: "wrap", alignItems: "center", justifyContent: "center", gap: "2.5vw", padding: "2.5vw" }}>
+                <img
+                  src={charts[selectedChart].src}
+                  alt={charts[selectedChart].alt}
+                  style={{ maxWidth: "600px", minWidth: "270px", border: "1px solid black", padding: "16px", borderRadius: "16px", width: "clamp(200px, 80%, 800px)" }}
+                />
+                <Typography component="span" sx={{ maxWidth: "600px", textAlign: "left", width: "clamp(200px, 80%, 800px)" }}>
+                  {charts[selectedChart].description}
+                </Typography>
+          </Box>
+        </Box>
+      ) : null}
+
+      {link ? (
+        <Box sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap-reverse",
+          width: "100%",
+          justifyContent:"space-between",
+          rowGap: "2.5vh",
+        }}>
+          <Button href={link} variant="outlined"
+          sx={{
+            padding: "10px 20px",
+            fontSize: "1.2rem",
+            fontWeight: "bold",
+          }}>
+            <Typography variant="caption" color={"primary"}
+            sx={{fontSize: "1.2rem", fontWeight: "bold"}}>
+              {header === 'The Impact' ? 'Learn More About The Problem' : 'Learn More'}
+            </Typography>
+          </Button>
+          {quoted && 
+       <Typography textAlign={"right"} paddingRight={"2.5vw"}>
+        <i>{quoted}</i>
+       </Typography>
+    
+        }
+
+        </Box>
+      ) : null} 
 
       
     </Box>
